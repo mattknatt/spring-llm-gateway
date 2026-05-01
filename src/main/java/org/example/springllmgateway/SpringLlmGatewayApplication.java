ee442ec2e86f0c45a5e220719294cf7e734462d9
mattknatt <mattiashagstrommusic@gmail.com>
2026-05-01 08:36:03 +0200

    Initial commit

---
+++ src/main/java/org/example/springllmgateway/SpringLlmGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.springllmgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLlmGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLlmGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springllmgateway/SpringLlmGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springllmgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLlmGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
